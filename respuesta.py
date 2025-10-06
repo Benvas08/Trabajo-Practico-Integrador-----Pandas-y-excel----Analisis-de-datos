56371438c54f4b3be89507a476f3cd8fe31d413d
Benvas08 <benjaminevasques@gmail.com>
2025-10-06 00:19:46 -0300

    Trabajo Practico Integrado -- Pandas y excel.   RESPUESTA

---
+++ respuesta.py
@@ -0,0 +1,47 @@
+import pandas as pd
+
+
+# -----------  1  -----------
+# -- Leemos las hojas y las guardamos en sus respectivos dataframes --
+df_1900s = pd.read_excel('data/movies.xls', sheet_name='1900s')
+df_2000s = pd.read_excel('data/movies.xls', sheet_name='2000s')
+df_2010s = pd.read_excel('data/movies.xls', sheet_name='2010s')
+
+# -- Mostramos las primeras 5 filas de cada dataframe y la cantidad de filas de cada uno --
+print("\nPrimeras 5 filas de la hoja 2000s:\n", df_2000s.head())
+print("\nCantidad de filas de la hoja 2010s:\n", len(df_2010s))
+
+
+# -----------  2  -----------
+# -- Concatenamos todos los dataframes en uno solo --
+df_movies = pd.concat([df_1900s, df_2000s, df_2010s], ignore_index=True)
+
+df_movies_10_filas = df_movies.head(10)
+print("\nPrimeras 10 filas de nuestro df_movies:\n", df_movies_10_filas)
+
+
+# -----------  3  -----------
+# -- Realizamos los análisis solicitados --
+print("\nCantidad de peliculas por país en df_movies:\n", df_movies['Country'].value_counts())
+print("\nTop 5 directores en 2010s:\n", df_2010s['Director'].value_counts().head(5))
+print("\nPeliculas por IMBD Score:\n", df_movies["IMDB Score"].sort_values(ascending=False).head())
+
+# -----------  4  -----------
+print("\nCalculamos el promedio IMDB Score por país:\n", df_movies.groupby('Country')["IMDB Score"].mean())
+
+def get_decade(year):
+    return str(year)[:3] + "0s" if pd.notna(year) else "Unknown"
+df_movies["Decade"] = df_movies["Year"].apply(get_decade)
+print("\nCantidad de peliculas por década:\n", df_movies["Decade"].value_counts())
+
+# -----------  5  -----------
+print("\nInformación general de df_movies:\n", df_movies.info())
+
+#print("Benjamin: Tene en cuenta que la funcion unknown_countr q estas aplicando a c/ elemento de la columna Country.Esta modificando a todos los valores.Por Ejm en United States se convierte en Uni en Mexico se convierte en Mex. la intension era conservar los nombres y en caso de haber NaN agregar un Unknown. \n La forma correcta de hacerlo es usando fillna('Unknown') q solo afecta a los valores nulos y conserva los demas nombres de paises.\n suegerencia :df_movies[nombreColumna] = df_movies[nombreColumna].fillna('Unknown')")
+def unknown_country(country):
+    return str(country)[:3]  if pd.notna(country) else "Unknown"
+df_movies_unknown = df_movies["Country"].apply(unknown_country)
+
+df_movies.to_excel('data/movies_unknow.xlsx', index=False)
+
+print("Valores faltantes reemplazados por unknown: \n", df_movies_unknown.value_counts())
